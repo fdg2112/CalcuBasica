ed078a6f32b9cdd72d954f17da579ce71f4e3bf4
fdg2112 <fdg2112@gmail.com>
2025-05-16 17:07:41 -0300

    act

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 const input1 = document.getElementById("display1");
 const input2 = document.getElementById("display2");
 const resultInput = document.getElementById("result");
+
+function calculate(operator) {
+    switch (operator) {
+        case '+':
+            resultInput.value = parseFloat(input1.value) + parseFloat(input2.value);
+            break;
+        case '-':
+            resultInput.value = parseFloat(input1.value) - parseFloat(input2.value);
+            break;
+        case '*':
+            resultInput.value = parseFloat(input1.value) * parseFloat(input2.value);
+            break;
+        case '/':
+            if (parseFloat(input2.value) !== 0) {
+                resultInput.value = parseFloat(input1.value) / parseFloat(input2.value);
+            } else {
+                alert("Cannot divide by zero");
+                resultInput.value = '';
+            }    
+    } 
+}
